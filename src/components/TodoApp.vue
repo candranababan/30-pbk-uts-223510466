76b7a7e86489eec203cbb19241a37d35bbda7009
candranababan <166162003+candranababan@users.noreply.github.com>
2024-05-14 12:13:17 +0700

    Update TodoApp.vue

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,13 +1,31 @@
 <template>
-  <div class ="container">
-    <h2 class ="text-center mt-5"> Daftar kegiatan </h2>
-
-    <!-- input -->
-    <div class="d-flex">
-      <input v-model="task" type="text" placeholder="Enter Task" class="form-control">
-      <button @click="submitTask" class="btn btn-warning rounded-0">SUBMIT</button>
+  <div class ="container-fluid" style="width: 40em">
+    <div class="card m-3">
+      <div class="card-header">
+        <ul class="nav nav-tabs">
+          <li class="nav-item" @click="selectedMenu = 'Todos'" :class="{ 'active': selectedMenu === 'Todos' }">
+            <a class="nav-link" href="#">Todos</a>
+          </li>
+          <li class="nav-item" @click="selectedMenu = 'Post'" :class="{ 'active': selectedMenu === 'Post' }">
+            <a class="nav-link" href="#">Post</a>
+          </li>
+        </ul>
+      </div>
+      <div class="card-body">
+        <div v-if="selectedMenu === 'Todos'">
+          <form @submit.prevent="addTodo">
+         </form>
+         </div>
+      </div>
     </div>
 
+    <!-- input -->
+    
+    <div class="d-flex">
+      <input v-model="task" type="text" placeholder="Masukkan List" class="form-control">
+      <button @click="submitTask" class="btn btn-warning rounded-0">TEKAN</button>
+    </div>
+    
     <!-- Task Table -->
     <table class="table table-bordered mt-5">
   <thead>
@@ -21,13 +39,13 @@
   <tbody>
     <tr v-for="(task, index) in tasks" :key="index">
       <td>
-        <span :class="{'finished': task.status === 'finished'}">{{task.name}}</span>
+        <span :class="{'selesai': task.status === 'selesai'}">{{task.name}}</span>
       </td>
       <td style="width: 120px">
         <span @click="changeStatus(index)" class="pointer"
-          :class="{'text-danger': task.status === 'to-do',
-          'text-warning': task.status === 'in-progress',
-          'text-success': task.status === 'finished'
+          :class="{'text-danger': task.status === 'melakukan',
+          'text-warning': task.status === 'proses melakukan',
+          'text-success': task.status === 'selesai'
           }"
         >
           {{ firstCharUpper(task.status) }}
@@ -38,6 +56,24 @@
           <span class="fa fa-pen"></span>
         </div>
       </td>
+      <ul class="list-group list-group-flush list-group-numbered">
+            <li class="list-group-item" v-for="todo in filteredTodos" :key="todo.title">
+              {{ todo.title }}
+            </li>
+          </ul>
+
+      <div v-if="selectedMenu === 'Post'">
+          <label for="userSelect">Select User:</label>
+          <select id="userSelect" v-model="selectedUser">
+            <option v-for="user in users" :key="user.id" :value="user.id">{{ user.name }}</option>
+          </select>
+          <ul v-if="selectedUser">
+            <li v-for="post in userPosts" :key="post.id">
+              {{ post.title }}
+            </li>
+          </ul>
+      </div>
+
       <td>
         <div class="text-center" @click="deleteTask(index)">
           <span class="fa fa-trash"></span>
@@ -46,10 +82,13 @@
     </tr>
   </tbody>
 </table>
-  </div>
+</div>  
+
+
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
   props: {
@@ -60,24 +99,22 @@ export default {
     return {
       task: '',
       editedTask: null,
-      availableStatuses: ['to-do', 'in-progress', 'finished'],
+      availableStatuses: ['melakukan', 'proses melaksanakan', 'selesai'],
 
-      tasks: [
-        {
-          name: 'Lari pagi 10 km',
-          status: 'to-do'
-        },
-        {
-          name: 'Diet dalam satu bulan',
-          status: 'in-progress'
-        },
-        {
-          name: 'jadwal kuliah jam 10 pagi',
-          status: 'finished'
-        }
-      ]
+      tasks: []
     }
   },
+
+  data2: () => ({
+    newTodo: '',
+    todos: [],
+    filter: null,
+    selectedMenu: 'Todos', // Default menu selection
+    users: [], // Placeholder for users data
+    selectedUser: null, // Selected user ID for posts
+    userPosts: [] // Placeholder for user posts data
+  }),
+
 
   methods: {
     submitTask(){
@@ -86,7 +123,7 @@ export default {
       if(this.editedTask === null){
         this.tasks.push({
           name: this.task,
-          status: 'to-do'
+          status: 'melakukan'
         });
       }else{
         this.tasks[this.editedTask].name = this.task;
@@ -113,7 +150,44 @@ export default {
 
     firstCharUpper(str){
       return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+
+    fetchUsers() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data2 => this.users = data2)
+        .catch(error => console.error('Error fetching users:', error));
+    },
+
+    fetchUserPosts(userId) {
+      fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+        .then(response => response.json())
+        .then(data2 => this.userPosts = data2)
+        .catch(error => console.error(`Error fetching posts for user ${userId}:`, error));
+    },
+  
+    watch: {
+    selectedUser(newVal) {
+      if (newVal) {
+        this.fetchUserPosts(newVal);
+      }
     }
+  },
+  created() {
+    this.fetchUsers();
+  },
+
+  computed: {
+    filteredTodos() {
+      if (this.filter === 'done') {
+        return this.todos.filter(todo => todo.done);
+      } else if (this.filter === 'undone') {
+        return this.todos.filter(todo => !todo.done);
+      } else {
+        return this.todos;
+      }
+    }
+  }
 
     }
   }
